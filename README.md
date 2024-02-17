7089e2b81107fae0e021a64fd16f61c9b489d9e3
Zane St. John <zstjohn@stanford.edu>
2024-02-17 14:32:18 -0800

    did the things

---
+++ README.md
@@ -0,0 +1,12 @@
+# hi tommy
+
+## get started
+
+1. have node js and npm installed on ur computer if you want to run this
+2. run `npm install -D` to install all dependencies
+3. create file `.env` in the root directory with the variable `OPENAI_API_KEY='<your api key here>'`
+4. run the project by running `npm start`
+
+## questions
+
+you know how to reach me
--- index.mjs
+++ index.mjs
@@ -1,8 +1,17 @@
+import path from "path";
 import OpenAI from "openai";
 import dotenv from "dotenv";
+import { existsSync, mkdirSync } from "fs";
+import { writeFile } from "fs/promises";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// grab env files
 dotenv.config();
 
+const OUTPUT_FOLDER_PATH = path.join(__dirname, "./output");
 const COMPANIES = ["Facebook", "Google", "Netflix", "Twitter"];
 
 const openai = new OpenAI();
@@ -11,19 +20,39 @@ function generateTemplate(companyName) {
   return `Write a poem about the company ${companyName}. Make it funny as hell.`;
 }
 
-function main() {
-  Promise.all(
-    COMPANIES.map(async (company) => {
+async function main() {
+  // Create images directory if not exists
+  if (!existsSync(OUTPUT_FOLDER_PATH)) {
+    mkdirSync(OUTPUT_FOLDER_PATH);
+  }
+
+  // Run queries concurrently
+  await Promise.all(
+    COMPANIES.map(async (companyName) => {
+      // Generate with OpenAI API
       const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: generateTemplate(company) }],
+        messages: [{ role: "system", content: generateTemplate(companyName) }],
         model: "gpt-3.5-turbo",
       });
 
-      console.log(`Poem for ${company}:`);
+      const responseText = completion.choices[0].message.content;
+
+      console.log(`[SUCCESS] Generated for ${companyName}:`);
       // Extra \n at the end
-      console.log(completion.choices[0].message.content + "\n");
+      console.log(responseText + "\n");
+
+      // Write to file
+      await writeFile(
+        // Replace spaces with _
+        // e.g. "My Company" => "My_Company.txt"
+        path.join(OUTPUT_FOLDER_PATH, `${companyName.replace(/ /g, "_")}.txt`),
+        responseText
+      );
     })
   );
+
+  // Ending message bc we're done
+  console.log("\n\n[DONE]");
 }
 
 main();
